df575ee31a21f62d5d497d2a61c1d1a533a4080e
Saurabh <saurabhsg1230@gmail.com>
2025-06-10 10:29:54 +0530

    shuffle button added

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
+
 import { useState } from 'react'
 // import './App.css'
 import data from './data'
 
-function App() {
-  // Helper to shuffle array
-  function fisherYatesShuffle(arr) {
-    for (let i = arr.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [arr[i], arr[j]] = [arr[j], arr[i]];
-    }
-    return arr;
+// Helper to shuffle array
+function fisherYatesShuffle(arr) {
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
   }
+  return arr;
+}
 
+
+function shuffle(data) {
   // Choose the center image (e.g., last image)
-  const centerIndex = 8; // Change this to fix a different image
+  const centerIndex = Math.floor(data.length / 2);// Change this to fix a different image
   const centerImage = data[centerIndex];
   const restImages = data.filter((_, idx) => idx !== centerIndex);
 
@@ -23,17 +25,26 @@ function App() {
   // Insert center image at the middle
   const middle = Math.floor(data.length / 2);
   shuffledRest.splice(middle, 0, centerImage);
+  return shuffledRest;
 
-  const elements = shuffledRest.map((item, idx) => (
+}
+
+function App() {
+  const shuffledRest = shuffle(data);
+  let [shuffle_rest, setShuffle] = useState(shuffledRest);
+  const elements = shuffle_rest.map((item, idx) => (
     <figure key={idx}>
       <img src={item.img} alt={item.name} />
       <figcaption>{item.name}</figcaption>
     </figure>
   ));
-
+function HandleClick(){
+  setShuffle(shuffle(shuffle_rest))
+}
   return (
     <div className='container'>
-      <h1>Image Gallary</h1>
+      <h1>Aimers</h1>
+      <button onClick={HandleClick} >Shuffle </button>
       <div className='gallary'>
         {elements}
       </div>
--- src/data.js
+++ src/data.js
@@ -1,38 +1,39 @@
 export default  [
     {
         name:"Mukund",
-        img:"/Images/image (1).jpeg"
+        img:"Images/image (1).jpeg"
     },
     {
         name:"Ritik",
-        img:"/Images/image (2).jpeg"
+        img:"Images/image (2).jpeg"
     },
     {
         name:"Rupesh",
-        img:"/Images/image (3).jpeg"
+        img:"Images/image (3).jpeg"
     },
+
     {
         name:"Shubham",
-        img:"/Images/image (4).jpeg"
+        img:"Images/image (4).jpeg"
+    },
+     {
+        name:"Saurabh",
+        img:"Images/image (9).jpeg"
     },
     {
         name:"Narasingh",
-        img:"/Images/image (5).jpeg"
+        img:"Images/image (5).jpeg"
     },
     {
         name:"Sainjit",
-        img:"/Images/image (6).jpeg"
+        img:"Images/image (6).jpeg"
     },
     {
         name:"Madhav",
-        img:"/Images/image (7).jpeg"
+        img:"Images/image (7).jpeg"
     },
     {
         name:"Amit",
-        img:"/Images/image (8).jpeg"
-    },
-    {
-        name:"Saurabh",
-        img:"/Images/image (9).jpeg"
+        img:"Images/image (8).jpeg"
     }
 ]
